559d69f080ac069aa1757bc63eb7ad836517352e
JamesDartmouth <jamessung76@gmail.com>
2022-08-18 18:58:05 -0700

    random generator updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // var slideEl = document.getElementById("slide")
 var quoteHistory = []
-var drinkHistory =[]
+var drinkHistory = []
 var sixPack = []
 
 
@@ -20,10 +20,10 @@ const options = {
 
 function selectDrink() {
 
-// TO BE TIED TO DROPDOWN?????????????????????????????????????????????????
+	// TO BE TIED TO DROPDOWN?????????????????????????????????????????????????
 
-	var liquor = "gin"
-	var drinkId = "15300"
+	var liquor = "tequila"
+
 
 
 	fetch('https://the-cocktail-db.p.rapidapi.com/filter.php?i=' + liquor, options)
@@ -31,27 +31,27 @@ function selectDrink() {
 			return response.json();
 		})
 		.then(function (data) {
-			// console.log(data.drinks[2])
-			// var liquorArr =[]
-			
-			// for( var i=0; i<data.drinks.length; i ++){
-			// 	liquorArr.push(JSON.stringify(data.drinks[i]))
-			// }
-			// console.log(parse(liquorArr))
+			var liquorArr = []
+			console.log(data)
+
+			for (var i = 0; i < data.drinks.length; i++) {
+				liquorArr.push(JSON.stringify(data.drinks[i]))
+			}
+			console.log(liquorArr)
 
 			// var liquorArr = [];
 			// var randomLiquor = Math.floor(Math.random() * liquorArr.length)
 			// var selectLiquor = liquorArr[randomLiquor]
 			// // var sixLiquor = []
 
-		// 	for (var i = 0; i < 6; i++) {
-		// 	sixLiquor.push(randomLiquor[i])
-		// 	}	
-		// 	for (var prop in data.drinks) {
-		// 		if (data.drinks[prop]) {
-		// 			liquorArr.push(data.drinks[prop]);
-		// 		}
-		// 	}
+			// 	for (var i = 0; i < 6; i++) {
+			// 	sixLiquor.push(randomLiquor[i])
+			// 	}	
+			// 	for (var prop in data.drinks) {
+			// 		if (data.drinks[prop]) {
+			// 			liquorArr.push(data.drinks[prop]);
+			// 		}
+			// 	}
 
 			var rand1 = data.drinks[0]
 			var rand2 = data.drinks[1]
@@ -60,78 +60,154 @@ function selectDrink() {
 			var rand5 = data.drinks[4]
 			var rand6 = data.drinks[5]
 
-			console.log(rand4)
-
-			document.getElementById("name1").textContent = drinkName
-			document.getElementById("drinkInstr").textContent = drinkInstruc
-			document.getElementById("pic1").src = data.drinks[0].strDrinkThumb
 
 
-		})
+			var drinkId = data.drinks[0].idDrink
 
-} 
-selectDrink();
+
+			document.getElementById("name1").textContent = data.drinks[0].strDrink;
+			document.getElementById("pic1").src = data.drinks[0].strDrinkThumb;
+			document.getElementById("card1").setAttribute("data-id", drinkId)
+
+			document.getElementById("name2").textContent = data.drinks[1].strDrink;
+			document.getElementById("pic2").src = data.drinks[1].strDrinkThumb;
+
+			document.getElementById("name3").textContent = data.drinks[2].strDrink;
+			document.getElementById("pic3").src = data.drinks[2].strDrinkThumb;
+
+			document.getElementById("name4").textContent = data.drinks[3].strDrink;
+			document.getElementById("pic4").src = data.drinks[3].strDrinkThumb;
+
+			document.getElementById("name5").textContent = data.drinks[4].strDrink;
+			document.getElementById("pic5").src = data.drinks[4].strDrinkThumb;
+
+			document.getElementById("name6").textContent = data.drinks[5].strDrink;
+			document.getElementById("pic6").src = data.drinks[5].strDrinkThumb;
+
+
+			
+			fetch('https://the-cocktail-db.p.rapidapi.com/lookup.php?i=' + drinkId, options)
+				.then(function (response) {
+					return response.json();
+				})
+				.then(function (data) {
+
+					var drinkData = data.drinks[0]
+					var drinkPic = data.drinks[0].strDrinkThumb
+					var drinkName = data.drinks[0].strDrink
+					var drinkInstruc = data.drinks[0].strInstructions
+					var ingredients = [];
+					var measures = [];
+					var ulEl = document.getElementById("drinkIngre")
+
+					for (var prop in drinkData) {
+						if (drinkData[prop]) {
+							if (prop.includes('strIngredient')) {
+								ingredients.push(drinkData[prop]);
+							} else if (prop.includes('strMeasure')) {
+								measures.push(drinkData[prop]);
+							}
+						}
+					}
+
+					for (var i = 0; i < ingredients.length; i++) {
+						var liEl = document.createElement('li');
+						liEl.textContent = measures[i] ? ingredients[i] + ': ' + measures[i] : ingredients[i];
+						ulEl.appendChild(liEl);
+					}
+
+					document.getElementById("drinkName").textContent = drinkName
+					document.getElementById("drinkInstr").textContent = drinkInstruc
+					document.getElementById("drinkImg").src = drinkPic
+				})
+			fetch('https://api.kanye.rest')
+				.then(function (response) {
+					return response.json();
+				})
+				.then(function (data) {
+					var quote = Object.values(data)
+					document.getElementById("quote").textContent = quote
+				})
+
+			// LOCAL STORAGE AT END OF FUNCITON???????????????????????????????????????
+			// var quoteHistory = [] this are in global scope already above
+			// var drinkHistory =[] this are in global scope already above
+
+			// localStorage.setItem("quote", quote)
+			// var pastQuote = localstorage.getItem("quote")
+			// console.log(quote)
+
+			// localStorage.setItem("drink", quote)
+			// var pastQuote = localstorage.getItem("drink")
+			// console.log(drink)	
+		}
+
+		)
+}
+
 
 // Need alt tags on picutres????????????????????????????
 // FUNCTION GETS SELECTED DRINK FROM 6 CHOICES
 
-var drinkId = "15300"
+// var drinkId = id1
 
-function getDrink() {
+// function getDrink(drinkId) {
+// 	console.log(typeof drinkId)
+// 	fetch('https://the-cocktail-db.p.rapidapi.com/lookup.php?i=' + drinkId, options)
+// 		.then(function (response) {
+// 			return response.json();
+// 		})
+// 		.then(function (data) {
 
-	fetch('https://the-cocktail-db.p.rapidapi.com/lookup.php?i=' + drinkId, options)
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (data) {
+// 			var drinkData = data.drinks[0]
+// 			var drinkPic = data.drinks[0].strDrinkThumb
+// 			var drinkName = data.drinks[0].strDrink
+// 			var drinkInstruc = data.drinks[0].strInstructions
+// 			var ingredients = [];
+// 			var measures = [];
+// 			var ulEl = document.getElementById("drinkIngre")
 
-			var drinkData = data.drinks[0]
-			var drinkPic = data.drinks[0].strDrinkThumb
-			var drinkName = data.drinks[0].strDrink
-			var drinkInstruc = data.drinks[0].strInstructions
-			var ingredients = [];
-			var measures = [];
-			var ulEl = document.getElementById("drinkIngre")
+// 			for (var prop in drinkData) {
+// 				if (drinkData[prop]) {
+// 					if (prop.includes('strIngredient')) {
+// 						ingredients.push(drinkData[prop]);
+// 					} else if (prop.includes('strMeasure')) {
+// 						measures.push(drinkData[prop]);
+// 					}
+// 				}
+// 			}
 
-			for (var prop in drinkData) {
-				if (drinkData[prop]) {
-					if (prop.includes('strIngredient')) {
-						ingredients.push(drinkData[prop]);
-					} else if (prop.includes('strMeasure')) {
-						measures.push(drinkData[prop]);
-					}
-				}
-			}
+// 			for (var i = 0; i < ingredients.length; i++) {
+// 				var liEl = document.createElement('li');
+// 				liEl.textContent = measures[i] ? ingredients[i] + ': ' + measures[i] : ingredients[i];
+// 				ulEl.appendChild(liEl);
+// 			}
 
-			for (var i = 0; i < ingredients.length; i++) {
-				var liEl = document.createElement('li');
-				liEl.textContent = measures[i] ? ingredients[i] + ': ' + measures[i] : ingredients[i];
-				ulEl.appendChild(liEl);
-			}
+// 			document.getElementById("drinkName").textContent = drinkName
+// 			document.getElementById("drinkInstr").textContent = drinkInstruc
+// 			document.getElementById("drinkImg").src = drinkPic
+// 		})
+// 	fetch('https://api.kanye.rest')
+// 		.then(function (response) {
+// 			return response.json();
+// 		})
+// 		.then(function (data) {
+// 			var quote = Object.values(data)
+// 			document.getElementById("quote").textContent = quote
+// 		})
 
-			document.getElementById("drinkName").textContent = drinkName
-			document.getElementById("drinkInstr").textContent = drinkInstruc
-			document.getElementById("drinkImg").src = drinkPic
-		})
-	fetch('https://api.kanye.rest')
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (data) {
-			var quote = Object.values(data)
-			document.getElementById("quote").textContent = quote
-		})
+// 	// LOCAL STORAGE AT END OF FUNCITON???????????????????????????????????????
+// 	// var quoteHistory = [] this are in global scope already above
+// 	// var drinkHistory =[] this are in global scope already above
 
-// LOCAL STORAGE AT END OF FUNCITON???????????????????????????????????????
+// 	// localStorage.setItem("quote", quote)
+// 	// var pastQuote = localstorage.getItem("quote")
+// 	// console.log(quote)
 
-	localStorage.setItem("quote", quote)
-	var pastQuote = localstorage.getItem("quote")
-	console.log(pastQuote)
-
-	localStorage.setItem("quote", quote)
-	var pastQuote = localstorage.getItem("quote")
-	console.log(pastQuote)	
-}
+// 	// localStorage.setItem("drink", quote)
+// 	// var pastQuote = localstorage.getItem("drink")
+// 	// console.log(drink)	
+// }
 
 
 //////FUNCTION FOR RANDOM DRINK---------------------------------------------
@@ -178,20 +254,58 @@ function randomDrink() {
 		.then(function (data) {
 			var quote = Object.values(data)
 			document.getElementById("quote").textContent = quote
+			console.log(data)
 		})
-
-// LOCAL STORAGE AT END OF FUNCITON???????????????????????????????
-
-	localStorage.setItem("quote", quote)
-	var pastQuote = localstorage.getItem("quote")
-	console.log(pastQuote)
-
-	localStorage.setItem("quote", quote)
-	var pastQuote = localstorage.getItem("quote")
-	console.log(pastQuote)
 }
 
-// WILLL RUN SELECT DRINK FUNCTION------------------------------
+
+// PREVIOUS AND NEXT ITEM FUNCTIONS:??????????????????????????????
+// var quoteHistory = [] this are in global scope already above
+// var drinkHistory =[] this are in global scope already above
+
+function prevItem() {
+	if (i === 0) {
+		i = arr.length;
+	}
+	i = i - 1
+
+	console.log(quoteHistory[i]);
+	console.log(drinkHistory[i]);
+
+	document.getElementById("quote").textContent = Object.values(quoteHistory[i])
+	document.getElementById("drinkName").textContent = drinkHistory[i].strDrink
+	document.getElementById("drinkInstr").textContent = drinkHistory[i].strInstructions
+	document.getElementById("drinkImg").src = drinkHistory[i].strDrinkThumb
+}
+function nextItem() {
+	i = i + 1
+	i = i % arr.length;
+
+	console.log(quoteHistory[i]);
+	console.log(drinkHistory[i]);
+
+	document.getElementById("quote").textContent = Object.values(quoteHistory[i])
+	document.getElementById("drinkName").textContent = drinkHistory[i].strDrink
+	document.getElementById("drinkInstr").textContent = drinkHistory[i].strInstructions
+	document.getElementById("drinkImg").src = drinkHistory[i].strDrinkThumb
+}
+
+
+
+// LOCAL STORAGE AT END OF FUNCITON???????????????????????????????
+// var quoteHistory = [] this are in global scope already above
+// var drinkHistory =[] this are in global scope already above
+
+// 	localStorage.setItem("quote", quote)
+// 	var pastQuote = localstorage.getItem("quote")
+// 	console.log(pastQuote)
+
+// 	localStorage.setItem("drink", quote)
+// 	var pastQuote = localstorage.getItem("drink")
+// 	console.log("drink")
+// }
+
+// // WILLL RUN SELECT DRINK FUNCTION------------------------------
 $("#barBtn").on('click', function (event) {
 	event.preventDefault();
 
@@ -206,57 +320,95 @@ $("#menuBtn").on('click', function (event) {
 	document.getElementById("greeting").style.display = "none";
 	document.getElementById("drink-box").style.display = "content";
 
-	getDrink();
+	selectDrink();
 })
 
 
-	// RETREIVE FROM LOCAL STORAGE?????????????????????????????????
-$("#prevBtn").on('click', function (event) {
+// // RETREIVE FROM LOCAL STORAGE?????????????????????????????????
+// $("#prevBtn").on('click', function (event) {
+// 	event.preventDefault();
+// 	document.getElementById("greeting").style.display = "none";
+// 	document.getElementById("drink-box").style.display = "content";
+// 	document.getElementById("drinkCards").style.display = "none";
+
+// 	prevItem();
+// })
+
+
+// // RETREIVE FROM LOCAL STORAGE???????????????????????????????
+// $("#nextBtn").on('click', function (event) {
+// 	event.preventDefault();
+// 	document.getElementById("greeting").style.display = "none";
+// 	document.getElementById("drink-box").style.display = "content";
+// 	document.getElementById("drinkCards").style.display = "none";
+
+// 	nextItem();
+
+// })
+
+// // WILL RETURN USER TO INTRO PAGE---------------------------------
+// $("#NewBtn").on('click', function (event) {
+// 	event.preventDefault();
+// 	document.getElementById("greeting").style.display = "content";
+// 	document.getElementById("drink-box").style.display = "none";
+// 	document.getElementById("drinkCards").style.display = "none";
+// })
+
+
+// // EVENT LISTENERS FOR 6 CHOICES----------------------------------
+
+$("#card1").on('click', function (event) {
 	event.preventDefault();
 
+	document.getElementById("drink-box").style.display = "content";
+	document.getElementById("drinkCards").style.display = "none";
+
+	var DrinkId = $("#card1").getAttribute
+
+	selectDrink();
 })
 
+// $("#card2").on('click', function (event) {
 
-	// RETREIVE FROM LOCAL STORAGE???????????????????????????????
-$("#nextBtn").on('click', function (event) {
-	event.preventDefault();
+// 	document.getElementById("drink-box").style.display = "content";
+// 	document.getElementById("drinkCards").style.display = "none";
+// 	event.preventDefault();
+// 	var DrinkId =
+// 		getDrink()
+// })
 
-})
+// $("#card3").on('click', function (event) {
 
-// WILL RETURN USER TO INTRO PAGE---------------------------------
-$("#NewBtn").on('click', function (event) {
-	event.preventDefault();
-	document.getElementById("greeting").style.display = "content";
-	document.getElementById("drink-box").style.display = "none";
-})
+// 	document.getElementById("drink-box").style.display = "content";
+// 	document.getElementById("drinkCards").style.display = "none";
+// 	event.preventDefault();
 
+// 	getDrink()
+// })
 
+// $("#card4").on('click', function (event) {
 
+// 	document.getElementById("drink-box").style.display = "content";
+// 	document.getElementById("drinkCards").style.display = "none";
+// 	event.preventDefault();
 
+// 	getDrink()
+// })
 
+// $("#card5").on('click', function (event) {
 
+// 	document.getElementById("drink-box").style.display = "content";
+// 	document.getElementById("drinkCards").style.display = "none";
+// 	event.preventDefault();
 
+// 	getDrink()
+// })
 
+// $("#card6").on('click', function (event) {
 
+// 	document.getElementById("drink-box").style.display = "content";
+// 	document.getElementById("drinkCards").style.display = "none";
+// 	event.preventDefault();
 
-
-
-
-
-
-
-// TO ALPHABETIZE
-// let array = ['cheese', 'corn', 'apple', 'acorn', 'beet', 'banana', 'yam', 'yucca']
-// let dict = array.reduce((a, c) => {
-//     // c[0] should be the first letter of an entry
-//     let k = c[0].toLocaleUpperCase()
-//     // either push to an existing dict entry or create one
-//     if (a[k]) a[k].push(c)
-//     else a[k] = [c]
-//     return a
-// }, {})
-// console.log(dict)
-// // Get the A's
-// console.log(dict['A'])
-
-
+// 	getDrink()
+// })
